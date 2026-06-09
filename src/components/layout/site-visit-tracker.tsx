1ab69fe2a763a37556d5e943c5bff73de7d332d6
Perfumaria9ilhas <perfumaria9ilhas@hotmail.com>
2026-06-09 22:17:58 +0000

    Adicionar Google Analytics

--- src/components/layout/site-visit-tracker.tsx
+++ src/components/layout/site-visit-tracker.tsx
@@ -1,32 +1,52 @@
 "use client";
 
+import Script from "next/script";
 import { useEffect } from "react";
 import { getAzoresDateKey } from "@/lib/date";
 
 const VISIT_KEY = "9ilhas-last-visit-date";
+const GA_ID = "G-VQ486TJ198";
 
 export function SiteVisitTracker() {
   useEffect(() => {
     const todayKey = getAzoresDateKey();
     const lastTracked = window.localStorage.getItem(VISIT_KEY);
 
-    if (lastTracked === todayKey) {
-      return;
-    }
-
-    void fetch("/api/visits/track", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then(() => {
-        window.localStorage.setItem(VISIT_KEY, todayKey);
+    if (lastTracked !== todayKey) {
+      void fetch("/api/visits/track", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch(() => {
-        // Ignore analytics failures so they never affect the storefront.
-      });
+        .then(() => {
+          window.localStorage.setItem(VISIT_KEY, todayKey);
+        })
+        .catch(() => {
+          // Ignorar erros
+        });
+    }
   }, []);
 
-  return null;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag(){
+            dataLayer.push(arguments);
+          }
+
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  );
 }
